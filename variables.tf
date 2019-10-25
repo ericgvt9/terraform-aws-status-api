52b653e37c4089b39c7a91bb4d87ceb5fa7c4e64
Raymond Yeh <yehjxraymond@gmail.com>
2019-10-25 13:52:00 +0800

    Added readme and default var

--- variables.tf
+++ variables.tf
@@ -1,15 +1,24 @@
 variable "aws_credentials_profile" {
-  type = "string"
+  type        = "string"
+  default     = "default"
+  description = "Profile used to deploy this infrastructure"
 }
 
 variable "aws_region" {
-  type = "string"
+  type        = "string"
+  default     = "ap-southeast-1"
+  description = "AWS region to deploy the infrastructure to"
 }
 
 variable "module_name" {
-  type = "string"
+  type        = "string"
+  default     = "status_api"
+  description = "Name of this module"
 }
 
 variable "stage" {
-  type = "string"
+  type        = "string"
+  default     = "dev"
+  description = "Stage to deploy to on AWS Gateway"
+
 }
