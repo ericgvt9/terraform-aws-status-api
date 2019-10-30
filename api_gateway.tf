af95a82dd796cea110601c1573eb7cf46936935d
Raymond Yeh <yehjxraymond@gmail.com>
2019-10-30 11:51:52 +0800

    idk what i doing

--- api_gateway.tf
+++ api_gateway.tf
@@ -16,6 +16,13 @@ resource "aws_api_gateway_resource" "status_id" {
   path_part   = "{id}"
 }
 
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id   = "${aws_api_gateway_resource.status_id.id}"
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_method" "get_status" {
   rest_api_id   = "${aws_api_gateway_rest_api.rest_api.id}"
   resource_id   = "${aws_api_gateway_resource.status_id.id}"
@@ -37,6 +44,43 @@ resource "aws_api_gateway_method" "post_status" {
   request_parameters = {
     "method.request.path.id" = true
   }
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.status_id.id}"
+  http_method = "${aws_api_gateway_method.options_method.http_method}"
+  status_code = 200
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+  depends_on = ["aws_api_gateway_method.options_method"]
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.status_id.id}"
+  http_method = "${aws_api_gateway_method.options_method.http_method}"
+  type        = "MOCK"
+  depends_on  = ["aws_api_gateway_method.options_method"]
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.status_id.id}"
+  http_method = "${aws_api_gateway_method.options_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.options_200.status_code}"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+  depends_on = ["aws_api_gateway_method_response.options_200"]
 }
 
 resource "aws_api_gateway_integration" "get_status_integration" {
